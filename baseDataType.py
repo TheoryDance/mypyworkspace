c3738568ee219b7ef3c76fc0347dbcc09caa12aa
TheoryDance <1378926190@qq.com>
2018-05-30 23:27:47 +0800

    Add files via upload

---
+++ baseDataType.py
@@ -0,0 +1,33 @@
+counter = 100
+miles = 1000.0
+name = "runoob"
+str = 'my name is ranfs'
+print(counter)
+print(miles)
+print(name)
+print(str)
+
+
+print("---------------test string equal---------------")
+name = 'hello,test equal'
+name2 = "hello,test equal"
+print(name == name2)
+print(name is name2)
+
+
+print("---------------test multiple variable---------------")
+a = b = c = 1
+print(a, b, c)
+
+a, b, c = 1, 2, "runoob"
+print(a, b, c)
+
+v = 4 + 3j
+print(v)
+
+boolean = True
+print(not (boolean and 5 == 4))
+
+print("---------------dictionary---------------")
+d = {"key1": "value1", "key2": "value2"}
+print(d['key2'])
